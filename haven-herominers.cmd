7ce2c5103340d5575a68429982a6b3fea1ca0732
cryptoself <81496126+cryptoself@users.noreply.github.com>
2021-03-27 23:35:51 -0400

    Add files via upload

---
+++ haven-herominers.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o haven.herominers.com:10451 -u hvxy9h5gzrXSTWig2HzDf6SzXuhwQVbbLXiTWeJApMX5dtQa6zNqUTFgarNAps8FwdUyFpvqh2roZ9fLmmx2n13i9Zuj3CDS59 -p YOUR_XMRRIG_NAME -a cn-heavy/xhv -k 
+pause
